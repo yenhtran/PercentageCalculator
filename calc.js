c8a81360be8f84de237b77ba6fe16772f6bc0f41
Yen Tran <tran.yentran@gmail.com>
2017-08-04 15:30:35 -0700

    add eventListeners

--- calc.js
+++ calc.js
@@ -1,3 +1,17 @@
 var numField1 = document.getElementById('numField1');
 var numField2 = document.getElementById('numField2');
-var resultField = document.getElementById('resultField') 
+var resultField = document.getElementById('resultField');
+var form = document.getElementById('xIsWhatPercentOfY');
+
+form.addEventListener('submit', function(){
+    
+    if (!numField1.value || !numField2.value) {
+        alert("Please enter values in the fields")
+    } else {
+        var x = parseFloat(numField1.value);
+        var y = parseFloat(numField2.value);
+        
+        alert(x+y);
+    }
+
+});
